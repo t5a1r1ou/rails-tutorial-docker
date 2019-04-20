e7e28c6837a42689078e5477da140e90d6054cfa
Shutto <baw.keepon69@gmail.com>
2019-04-20 21:24:44 +0900

    implement basic login

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
+gem 'jquery-rails', '4.3.1'
 gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
@@ -7,6 +9,11 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   resources :users
 
 end
